ccdeef1ea9a1abe10f2c802c7d4c56ad8adfc848
Michael Thielemans <michaelthielemans@gmail.com>
2024-10-18 19:35:54 +0200

    ok

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config(object):
     SECRET_KEY = 'do-or-do-not-there-is-no-try'
 #    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
 # version with sqlite embeddid in the same container    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://user:password@mariadb_container/dbname'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
